f55395cb346d30d3b6ade2ff8f58ab89cb1611a1
John Rhoden <jrho19.jr@gmail.com>
2016-08-11 12:05:38 -0400

    models for chatroom and messages and user added

--- db/migrate/20160811152651_create_chatrooms.rb
+++ db/migrate/20160811152651_create_chatrooms.rb
@@ -2,7 +2,7 @@ class CreateChatrooms < ActiveRecord::Migration[5.0]
   def change
     create_table :chatrooms do |t|
       t.string :topic
-      t.has_many :messages
+      t.string :messages
 
       t.timestamps
     end
